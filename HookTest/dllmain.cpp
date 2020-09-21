7fa1307d0ebf07caa81a8a07b01fdf9d533d43f4
전고흐 <35092759+0silver00@users.noreply.github.com>
2020-09-21 22:55:40 +0900

    디투어 예제파일

--- HookTest/dllmain.cpp
+++ HookTest/dllmain.cpp
@@ -1,5 +1,18 @@
 ﻿// dllmain.cpp : DLL 애플리케이션의 진입점을 정의합니다.
 #include "pch.h"
+#include <detours.h>
+#include <stdio.h>
+#include <iostream>
+
+// 속도 확인하면서 명시적으로 바꿀 필요성 있을듯함
+#define DLLBASIC_API extern "C" __declspec(dllexport)
+#pragma comment(lib, "detours.lib")
+
+static INT(WINAPI* TrueMessageBoxA)(HWND hWnd, LPCSTR lpTextm, LPCSTR lpCaption, UINT uType) = MessageBoxA;
+
+DLLBASIC_API INT WINAPI HookTestMessageBoxA(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType) {
+    return TrueMessageBoxA(hWnd, "Hooking", lpCaption, uType);
+}
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -9,8 +22,22 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+        DetourRestoreAfterWith();
+        //DisableTreadLibraryCalls(hModule);
+
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourAttach(&(PVOID&)TrueMessageBoxA, HookTestMessageBoxA);
+        DetourTransactionCommit();
+        break;
     case DLL_THREAD_ATTACH:
+        break;
     case DLL_THREAD_DETACH:
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourDetach(&(PVOID&)TrueMessageBoxA, HookTestMessageBoxA);
+        DetourTransactionCommit();
+        break;
     case DLL_PROCESS_DETACH:
         break;
     }
