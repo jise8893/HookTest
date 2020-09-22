acd467af887f7004273de2c5e6a7c4df4d6b305d
전고흐 <35092759+0silver00@users.noreply.github.com>
2020-09-22 22:53:22 +0900

    null

--- HookTest/dllmain.cpp
+++ HookTest/dllmain.cpp
@@ -1,17 +1,23 @@
 ﻿// dllmain.cpp : DLL 애플리케이션의 진입점을 정의합니다.
 #include "pch.h"
 #include <detours.h>
-#include <stdio.h>
-#include <iostream>
 
 // 속도 확인하면서 명시적으로 바꿀 필요성 있을듯함
 #define DLLBASIC_API extern "C" __declspec(dllexport)
 #pragma comment(lib, "detours.lib")
 
-static INT(WINAPI* TrueMessageBoxA)(HWND hWnd, LPCSTR lpTextm, LPCSTR lpCaption, UINT uType) = MessageBoxA;
+//static INT(WINAPI* TrueMessageBoxA)(HWND hWnd, LPCSTR lpTextm, LPCSTR lpCaption, UINT uType) = MessageBoxA;
+//DLLBASIC_API INT WINAPI HookTestMessageBoxA(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType) {
+//    return TrueMessageBoxA(hWnd, "Hooking", lpCaption, uType);
+//}
 
-DLLBASIC_API INT WINAPI HookTestMessageBoxA(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType) {
-    return TrueMessageBoxA(hWnd, "Hooking", lpCaption, uType);
+// TrueVirtualAllocEx = VirtualAllocEx를 지정해줍니다.
+static LPVOID(WINAPI* TrueVirtualAllocEx)(HANDLE hProcess, LPVOID lpAddress, SIZE_T dwSize, DWORD flAllocationType, DWORD flProtect) = VirtualAllocEx;
+
+DLLBASIC_API LPVOID WINAPI HookVirtualAllocEx(HANDLE hProcess, LPVOID lpAddress, SIZE_T dwSize, DWORD flAllocationType, DWORD flProtect) {
+    OutputDebugString(L"VirtualAllocEx Hooking SUCCESS!");
+
+    return TrueVirtualAllocEx(hProcess, lpAddress, dwSize, flAllocationType, flProtect);
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
@@ -27,17 +33,23 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
-        DetourAttach(&(PVOID&)TrueMessageBoxA, HookTestMessageBoxA);
-        DetourTransactionCommit();
+        //프로세스가 실제 함수 대신 후킹 함수를 호출해야한다고 믿게만듭니다.
+        DetourAttach(&(PVOID&)TrueVirtualAllocEx, HookVirtualAllocEx);
+        //DetourTransactionCommit();
+        if (DetourTransactionCommit() != NO_ERROR) {
+            OutputDebugString(L"VirtualAllocEx detoured UNsuccessfully");
+        }
+        else
+            OutputDebugString(L"VirtualAllocEx detoured successfully");
+        break;
         break;
     case DLL_THREAD_ATTACH:
         break;
     case DLL_THREAD_DETACH:
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
-        DetourDetach(&(PVOID&)TrueMessageBoxA, HookTestMessageBoxA);
+        DetourDetach(&(PVOID&)TrueVirtualAllocEx, HookVirtualAllocEx);
         DetourTransactionCommit();
-        break;
     case DLL_PROCESS_DETACH:
         break;
     }
