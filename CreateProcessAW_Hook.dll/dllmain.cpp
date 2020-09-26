dfce8f2501e2b5b0790624817703a1b82409f5bf
jise8893 <60957575+jise8893@users.noreply.github.com>
2020-09-26 18:31:35 +0900

    Add files via upload

---
+++ CreateProcessAW_Hook.dll/dllmain.cpp
@@ -0,0 +1,129 @@
+﻿#include "pch.h"
+#include <detours.h>
+#include <stdio.h>
+#include <processthreadsapi.h>
+
+#define DLLBASIC_API extern "C" __declspec(dllexport)
+#define HOOKDLL_PATH "C:\\Fast.dll"  // DLL경로
+#pragma comment(lib, "detours.lib")
+
+static BOOL(WINAPI* TrueCreateProcessA)(
+    LPCSTR                lpApplicationName,
+    LPSTR                 lpCommandLine,
+    LPSECURITY_ATTRIBUTES lpProcessAttributes,
+    LPSECURITY_ATTRIBUTES lpThreadAttributes,
+    BOOL                  bInheritHandles,
+    DWORD                 dwCreationFlags,
+    LPVOID                lpEnvironment,
+    LPCSTR                lpCurrentDirectory,
+    LPSTARTUPINFOA        lpStartupInfo,
+    LPPROCESS_INFORMATION lpProcessInformation
+    ) = CreateProcessA;
+
+
+static BOOL(WINAPI* TrueCreateProcessW)(
+    LPCWSTR               lpApplicationName,
+    LPWSTR                lpCommandLine,
+    LPSECURITY_ATTRIBUTES lpProcessAttributes,
+    LPSECURITY_ATTRIBUTES lpThreadAttributes,
+    BOOL                  bInheritHandles,
+    DWORD                 dwCreationFlags,
+    LPVOID                lpEnvironment,
+    LPCWSTR               lpCurrentDirectory,
+    LPSTARTUPINFOW        lpStartupInfo,
+    LPPROCESS_INFORMATION lpProcessInformation
+    ) = CreateProcessW;
+
+
+DLLBASIC_API BOOL WINAPI HookCreateProcessA(
+    LPCSTR                lpApplicationName,
+    LPSTR                 lpCommandLine,
+    LPSECURITY_ATTRIBUTES lpProcessAttributes,
+    LPSECURITY_ATTRIBUTES lpThreadAttributes,
+    BOOL                  bInheritHandles,
+    DWORD                 dwCreationFlags,
+    LPVOID                lpEnvironment,
+    LPCSTR                lpCurrentDirectory,
+    LPSTARTUPINFOA        lpStartupInfo,
+    LPPROCESS_INFORMATION lpProcessInformation)
+{
+
+    return DetourCreateProcessWithDllExA(lpApplicationName,
+        lpCommandLine,
+        lpProcessAttributes,
+        lpThreadAttributes,
+        bInheritHandles,
+        dwCreationFlags,
+        lpEnvironment,
+        lpCurrentDirectory,
+        lpStartupInfo,
+        lpProcessInformation,
+        HOOKDLL_PATH,
+        TrueCreateProcessA);
+}
+
+DLLBASIC_API BOOL WINAPI HookCreateProcessW(
+    LPCWSTR               lpApplicationName,
+    LPWSTR                lpCommandLine,
+    LPSECURITY_ATTRIBUTES lpProcessAttributes,
+    LPSECURITY_ATTRIBUTES lpThreadAttributes,
+    BOOL                  bInheritHandles,
+    DWORD                 dwCreationFlags,
+    LPVOID                lpEnvironment,
+    LPCWSTR               lpCurrentDirectory,
+    LPSTARTUPINFOW        lpStartupInfo,
+    LPPROCESS_INFORMATION lpProcessInformation
+)
+{
+
+    return DetourCreateProcessWithDll(lpApplicationName,
+        lpCommandLine,
+        lpProcessAttributes,
+        lpThreadAttributes,
+        bInheritHandles,
+        dwCreationFlags,
+        lpEnvironment,
+        lpCurrentDirectory,
+        lpStartupInfo,
+        lpProcessInformation,
+        HOOKDLL_PATH,
+        TrueCreateProcessW);
+    
+}
+
+
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
+)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        DetourRestoreAfterWith();
+        //DisableTreadLibraryCalls(hModule);
+        printf("\nDll_process_attach\n");
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourAttach(&(PVOID&)TrueCreateProcessA, HookCreateProcessA);
+        DetourAttach(&(PVOID&)TrueCreateProcessW, HookCreateProcessW);
+        DetourTransactionCommit();
+        break;
+    case DLL_THREAD_ATTACH:
+        printf("DLL_THREAD_ATTACH\n");
+        break;
+    case DLL_THREAD_DETACH:
+        printf("DLL_THREAD_DETACH\n");
+        break;
+    case DLL_PROCESS_DETACH:
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourDetach(&(PVOID&)TrueCreateProcessA, HookCreateProcessA);
+        DetourDetach(&(PVOID&)TrueCreateProcessW, HookCreateProcessW);
+        DetourTransactionCommit();
+        printf("DLL_PROCESS_DETACH\n");
+        break;
+    }
+    return TRUE;
+}
